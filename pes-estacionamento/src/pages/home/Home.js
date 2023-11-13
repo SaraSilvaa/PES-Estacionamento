31f640f619acaf75c47bcc19577a41aed0e99511
SaraSilvaa <141691931+SaraSilvaa@users.noreply.github.com>
2023-11-13 12:12:51 +0000

    deu erro api

--- pes-estacionamento/src/pages/home/Home.js
+++ pes-estacionamento/src/pages/home/Home.js
@@ -1,153 +1,126 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SidebarMenu from '../../components/sidebar/sidebar';
 import Buscar from '../../img/icone_buscarcarros.png';
-// import Faturar from '../../img/icone_faturamento.png';
 import './StyleHome.css';
 import axios from 'axios';
 
 export const Cadastro = () => {
- const [page, setPage] = useState('Home');
- const changePage = (newPage) => {
+  const [page, setPage] = useState('Home');
+  const changePage = (newPage) => {
     setPage(newPage);
- };
- const [tipoAutomovel, setTipo] = useState('');
- const [placa, setPlaca] = useState('');
- const [cpfcnpj, setCpfCnpj] = useState('');
- const [status, setStatus] = useState('');
-//  const [tempoSaida, setSaida] = useState('');
- const [valor, setvalor] = useState('');
-//  const [tempoEntrada, setEntrada] = useState('');
- const [ano, setAno] = useState('');
- const [nome, setNome] = useState('');
- const [cadastroStatus, setCadastroStatus] = useState(null);
+  };
+  const [tipoAutomovel, setTipo] = useState('');
+  const [placa, setPlaca] = useState('');
+  const [cpfcnpj, setCpfCnpj] = useState('');
+  const [status] = useState('Ativo');
+  const [cadastroStatus, setCadastroStatus] = useState(null);
+  const [contato, setContatos] = useState([]);
 
- const getHoraAtual = () => {
-  const horaAtual = new Date();
-  return `${horaAtual.getHours()}:${horaAtual.getMinutes()}`;
-};
-const getHoraAtual2 = () => {
-  const horaAtual = new Date();
-  return `${horaAtual.getHours()}:${horaAtual.getMinutes()}`;
-};
+  useEffect(() => {
+    pesquisarVeiculos();
+  }, []);
 
- const handleSubmit = async (e) => {
-  e.preventDefault();
+  const getHoraAtual = () => {
+    const horaAtual = new Date();
+    const horas = horaAtual.getHours();
+    const minutos = horaAtual.getMinutes();
+    const segundos = horaAtual.getSeconds();
+  
+    // Formate os valores para garantir que sempre tenham dois dígitos
+    const formatadoHoras = horas < 10 ? `0${horas}` : horas;
+    const formatadoMinutos = minutos < 10 ? `0${minutos}` : minutos;
+    const formatadoSegundos = segundos < 10 ? `0${segundos}` : segundos;
+  
+    return `${formatadoHoras}:${formatadoMinutos}:${formatadoSegundos}`;
+  };
+  
 
-  try {
-    const response = await axios.post('https://web-nf32wtl2j0zn.up-de-fra1-1.apps.run-on-seenode.com/automovel', {
-      tipoAutomovel,
-      placa,
-      cpfcnpj,
-      status,
-      tempoEntrada: getHoraAtual(),
-      tempoSaida: getHoraAtual2(),
-      valor,
-      ano,
-      nome,
-    });
-    
-    if (response.status === 200 || response.status === 201) {
-      // Cadastro bem-sucedido
-      setCadastroStatus('Cadastro realizado com sucesso.');
-      // Exibe a caixa de alerta
-      alert('Cadastro realizado com sucesso.');
-    } else {
-      // Tratando erros de acordo com a resposta do servidor
+  const pesquisarVeiculos = async () => {
+    try {
+      const response = await axios.get('https://web-nf32wtl2j0zn.up-de-fra1-1.apps.run-on-seenode.com/automovel');
+
+      if (response.status === 200) {
+        setContatos(response.data);
+      } else {
+        console.log('Erro ao buscar veículos:', response.status);
+      }
+    } catch (error) {
+      console.error('Erro ao buscar veículos:', error);
+    }
+  };
+
+  const finalizarRegistro = async (id) => {
+    try {
+      const response = await axios.delete(`https://web-nf32wtl2j0zn.up-de-fra1-1.apps.run-on-seenode.com/automovel${id}`);
+
+      if (response.status === 200) {
+        console.log(`Registro com o ID ${id} finalizado com sucesso.`);
+        pesquisarVeiculos();
+      } else {
+        console.error(`Erro ao finalizar o registro com o ID ${id}.`);
+      }
+    } catch (error) {
+      console.error(`Erro ao finalizar o registro com o ID ${id}.`, error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('https://web-nf32wtl2j0zn.up-de-fra1-1.apps.run-on-seenode.com/automovel', {
+        tipoAutomovel,
+        placa,
+        cpfcnpj,
+        status,
+        tempoEntrada: getHoraAtual(),
+      });
+
+      if (response.status === 200 || response.status === 201) {
+        setCadastroStatus('Cadastro realizado com sucesso.');
+        alert('Cadastro realizado com sucesso.');
+        pesquisarVeiculos();
+      } else {
+        setCadastroStatus('Erro ao cadastrar. Por favor, tente novamente.');
+        alert('Erro ao cadastrar. Por favor, tente novamente.');
+      }
+    } catch (error) {
+      console.error(error);
       setCadastroStatus('Erro ao cadastrar. Por favor, tente novamente.');
-      // Exibe a caixa de alerta
       alert('Erro ao cadastrar. Por favor, tente novamente.');
     }
-  } catch (error) {
-    console.error(error);
-    setCadastroStatus('Erro ao cadastrar. Por favor, tente novamente.');
-    // Exibe a caixa de alerta
-    alert('Erro ao cadastrar. Por favor, tente novamente.');
-  }
-};
-const [contato, setContatos] = useState([]);
+  };
 
-const pesquisarVeiculos = async () => {
-  try {
-    const response = await axios.get('https://web-nf32wtl2j0zn.up-de-fra1-1.apps.run-on-seenode.com/automovel');
+  return (
+    <div>
+      <SidebarMenu changePage={changePage} />
+      {page === 'Cadastrar' && (
+        <div className='FormInput'>
+          <h1>Cadastrar</h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Tipo de Automóvel:
+              <input className='cadastroInput' type="text" value={tipoAutomovel} onChange={(e) => setTipo(e.target.value)} />
+            </label>
+            <label>
+              Placa:
+              <input className='cadastroInput' type="text" value={placa} onChange={(e) => setPlaca(e.target.value)} />
+            </label>
+            <label>
+              CPF/CNPJ:
+              <input className='cadastroInput' type="text" value={cpfcnpj} onChange={(e) => setCpfCnpj(e.target.value)} />
+            </label>
+            <label>
+              Hora de Entrada:
+              <input className='cadastroInput' type="text" value={getHoraAtual()} readOnly />
+            </label>
+            <button className='EnviarCadastro' type="submit">Cadastrar</button>
+          </form>
+          {cadastroStatus && <p>{cadastroStatus}</p>}
+        </div>
+      )}
 
-    if (response.status === 200) {
-      // Os veículos foram encontrados com sucesso
-      setContatos(response.data); // Atualize o estado com os veículos encontrados
-    } else {
-      // Trate erros de acordo com a resposta do servidor
-      console.log('Erro ao buscar veículos.');
-    }
-  } catch (error) {
-    console.error(error);
-    console.log('Erro ao buscar veículos.');
-  }
-};
-
-// Função para lidar com o clique no botão "Finalizar"
-const finalizarRegistro = async (id) => {
-  try {
-    // Realiza a chamada à API para excluir o registro com o ID fornecido
-    const response = await axios.delete(`https://web-nf32wtl2j0zn.up-de-fra1-1.apps.run-on-seenode.com/automovel${id}`);
-
-    if (response.status === 200) {
-      console.log(`Registro com o ID ${id} finalizado com sucesso.`);
-      // Atualiza a lista de contatos após a exclusão bem-sucedida (opcional)
-      pesquisarVeiculos();
-    } else {
-      console.error(`Erro ao finalizar o registro com o ID ${id}.`);
-    }
-  } catch (error) {
-    console.error(`Erro ao finalizar o registro com o ID ${id}.`, error);
-  }
-};
-
-const renderContent = () => {
-  if (page === 'Cadastrar') {
-     return (
-       <div className='FormInput'>
-         <h1>Cadastrar</h1>
-         <form onSubmit={handleSubmit}>
-           <label>
-             Tipo de Automóvel:
-             <input className='cadastroInput' type="text" value={tipoAutomovel} onChange={(e) => setTipo(e.target.value)} />
-           </label>
-           <label>
-             Placa:
-             <input className='cadastroInput' type="text" value={placa} onChange={(e) => setPlaca(e.target.value)} />
-           </label>
-           <label>
-             CPF/CNPJ:
-             <input className='cadastroInput' type="text" value={cpfcnpj} onChange={(e) => setCpfCnpj(e.target.value)} />
-           </label>
-           <label>
-             Status:
-             <input className='cadastroInput' type="text" value={status} onChange={(e) => setStatus(e.target.value)} />
-           </label>
-           <label>
-             Tempo de Entrada:
-                  <input className='cadastroInput' type="text" value={getHoraAtual()} readOnly />
-           </label>
-           <label>
-             Valor:
-             <input className='cadastroInput' type="text" value={valor} onChange={(e) => setvalor(e.target.value)} />
-           </label>
-           <label>
-             Ano:
-             <input className='cadastroInput' type="text" value={ano} onChange={(e) => setAno(e.target.value)} />
-           </label>
-           <label>
-             Nome:
-             <input className='cadastroInput' type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
-           </label>
-           <button className='EnviarCadastro' type="submit">Cadastrar</button>
-         </form>
-         {cadastroStatus && <p>{cadastroStatus}</p>}
-       </div>
-      );
-     
-     } else if (page === 'Pesquisar') {
-      pesquisarVeiculos();
-      return (
+      {page === 'Pesquisar' && (
         <div className='FormTable'>
           <h1 className='title'>Pesquisar Veículos <img src={Buscar} className='IconeTela' alt='logo' /></h1>
           <div className='Pesquisa'>
@@ -166,19 +139,23 @@ const renderContent = () => {
                 <th>CPF/CNPJ</th>
                 <th>Hora Entrada</th>
                 <th>Status</th>
-                <th>Ações</th> {/* Nova coluna para os botões */}
+                <th>Ações</th>
               </tr>
             </thead>
             <tbody>
-              {contato.map((contato, index) => (
+              {contato?.map?.((contato, index) => (
                 <tr key={index}>
                   <td>{contato.tipoAutomovel}</td>
                   <td>{contato.placa}</td>
                   <td>{contato.cpfcnpj}</td>
                   <td>{contato.tempoEntrada}</td>
-                  <td>{contato.status}</td>
                   <td>
-                    {/* Adiciona um botão "Finalizar" para cada registro */}
+                    {contato.status.toLowerCase() === 'ativo' ? (
+                      <span style={{ color: 'green', marginRight: '8px' }}>●</span>
+                    ) : null}
+                    {contato.status}
+                  </td>
+                  <td>
                     <button onClick={() => finalizarRegistro(contato.id)}>Finalizar</button>
                   </td>
                 </tr>
@@ -186,17 +163,9 @@ const renderContent = () => {
             </tbody>
           </table>
         </div>
-      );
-   
-     } else if (page === 'Faturar') {
-       return <div className='title'>Contact Content</div>;
-     }
-  };
+      )}
 
-  return (
-     <div>
-       <SidebarMenu changePage={changePage} />
-       {renderContent()}
-     </div>
+      {page === 'Faturar' && <div className='title'>Contact Content</div>}
+    </div>
   );
 };

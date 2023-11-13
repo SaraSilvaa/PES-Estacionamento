9a4af441d0b3f96aed8a1e1603656d93187f1f62
SaraSilvaa <141691931+SaraSilvaa@users.noreply.github.com>
2023-11-13 13:21:50 +0000

    Modal fechamento

--- pes-estacionamento/src/pages/home/Home.js
+++ pes-estacionamento/src/pages/home/Home.js
@@ -3,6 +3,7 @@ import SidebarMenu from '../../components/sidebar/sidebar';
 import Buscar from '../../img/icone_buscarcarros.png';
 import './StyleHome.css';
 import axios from 'axios';
+import { format } from 'date-fns';
 
 export const Cadastro = () => {
   const [page, setPage] = useState('Home');
@@ -15,8 +16,12 @@ export const Cadastro = () => {
   const [status] = useState('Ativo');
   const [cadastroStatus, setCadastroStatus] = useState(null);
   const [contato, setContatos] = useState([]);
-  const [valor, setValor ] = useState(null);
+  const [valor, setValor] = useState(null);
+  const getDataAtual = () => {
+    return format(new Date(), 'dd-MM-yyyy');
+  };
 
+  const [data] = useState(getDataAtual());
 
   useEffect(() => {
     pesquisarVeiculos();
@@ -26,14 +31,12 @@ export const Cadastro = () => {
     const horaAtual = new Date();
     const horas = horaAtual.getHours();
     const minutos = horaAtual.getMinutes();
-  
-    // Formate os valores para garantir que sempre tenham dois dígitos
+
     const formatadoHoras = horas < 10 ? `0${horas}` : horas;
     const formatadoMinutos = minutos < 10 ? `0${minutos}` : minutos;
-  
+
     return `${formatadoHoras}:${formatadoMinutos}`;
   };
-  
 
   const pesquisarVeiculos = async () => {
     try {
@@ -52,21 +55,23 @@ export const Cadastro = () => {
   const finalizarRegistro = async (id) => {
     try {
       const response = await axios.delete(`https://web-nf32wtl2j0zn.up-de-fra1-1.apps.run-on-seenode.com/automovel${id}`);
-
+  
       if (response.status === 200) {
         console.log(`Registro com o ID ${id} finalizado com sucesso.`);
         pesquisarVeiculos();
+        closeModal(); // Close the modal after finalizing
       } else {
-        console.error(`Erro ao finalizar o registro com o ID ${id}.`);
+        console.error(`Erro ao finalizar o registro com o ID ${id}. Status: ${response.status}`);
       }
     } catch (error) {
       console.error(`Erro ao finalizar o registro com o ID ${id}.`, error);
     }
   };
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
       const response = await axios.post('https://web-nf32wtl2j0zn.up-de-fra1-1.apps.run-on-seenode.com/automovel', {
         tipoAutomovel,
@@ -75,13 +80,14 @@ export const Cadastro = () => {
         status,
         tempoEntrada: getHoraAtual(),
         valor,
+        data,
       });
-  
+
       if (response.status === 200 || response.status === 201) {
         setCadastroStatus('Cadastro realizado com sucesso.');
         alert('Cadastro realizado com sucesso.');
         pesquisarVeiculos();
-        setValor(null); // Limpa o campo valor após o cadastro
+        setValor(null);
       } else {
         setCadastroStatus('Erro ao cadastrar. Por favor, tente novamente.');
         alert('Erro ao cadastrar. Por favor, tente novamente.');
@@ -90,6 +96,54 @@ export const Cadastro = () => {
       console.log(error.response);
       setCadastroStatus('Erro ao cadastrar. Por favor, tente novamente.');
       alert('Erro ao cadastrar. Por favor, tente novamente.');
+    }
+  };
+
+  // Modal state and functions
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [itemIdToFinalize, setItemIdToFinalize] = useState(null);
+  const getPlacaById = (id) => {
+    const selectedItem = contato.find(item => item.id === id);
+    return selectedItem ? selectedItem.placa : 'Placa não encontrada';
+  };
+  
+
+  const closeModal = () => {
+    setItemIdToFinalize(null);
+    setIsModalOpen(false);
+  };
+  const getCpfCnpjById = (id) => {
+    const selectedItem = contato.find(item => item.id === id);
+    return selectedItem ? selectedItem.cpfcnpj : 'CPF/CNPJ não encontrado';
+  };
+  const openModal = (id) => {
+    setItemIdToFinalize(id);
+    setIsModalOpen(true);
+  };
+  
+  
+  const confirmFinalize = async () => {
+    try {
+      if (itemIdToFinalize !== null) {
+        const selectedItem = contato.find(item => item.id === itemIdToFinalize);
+  
+        if (selectedItem) {
+          const { placa, cpfcnpj } = selectedItem;
+          console.log(`Finalizando estadia do cliente com Placa ${placa} e CNPJ/CPF ${cpfcnpj}`);
+  
+          // Perform finalization logic
+          await finalizarRegistro(itemIdToFinalize);
+  
+          // Close the modal
+          closeModal();
+        } else {
+          console.error(`Erro ao encontrar o item com ID ${itemIdToFinalize}.`);
+        }
+      } else {
+        console.error('Nenhum item selecionado para finalizar.');
+      }
+    } catch (error) {
+      console.error('Erro ao finalizar estadia do cliente:', error);
     }
   };
 
@@ -111,6 +165,10 @@ export const Cadastro = () => {
             <label>
               CPF/CNPJ:
               <input className='cadastroInput' type="text" value={cpfcnpj} onChange={(e) => setCpfCnpj(e.target.value)} />
+            </label>
+            <label>
+              Data:
+              <input className='cadastroInput' type="text" value={getDataAtual()} readOnly />
             </label>
             <label>
               Hora de Entrada:
@@ -139,6 +197,7 @@ export const Cadastro = () => {
                 <th>Modelo</th>
                 <th>Placa</th>
                 <th>CPF/CNPJ</th>
+                <th>Data</th>
                 <th>Hora Entrada</th>
                 <th>Status</th>
                 <th>Ações</th>
@@ -150,6 +209,7 @@ export const Cadastro = () => {
                   <td>{contato.tipoAutomovel}</td>
                   <td>{contato.placa}</td>
                   <td>{contato.cpfcnpj}</td>
+                  <td>{contato.data}</td>
                   <td>{contato.tempoEntrada}</td>
                   <td>
                     {contato.status.toLowerCase() === 'ativo' ? (
@@ -158,7 +218,7 @@ export const Cadastro = () => {
                     {contato.status}
                   </td>
                   <td>
-                    <button onClick={() => finalizarRegistro(contato.id)}>Finalizar</button>
+                    <button className='EnviarCadastro' onClick={() => openModal(contato.id)}>Finalizar</button>
                   </td>
                 </tr>
               ))}
@@ -168,6 +228,18 @@ export const Cadastro = () => {
       )}
 
       {page === 'Faturar' && <div className='title'>Contact Content</div>}
+
+      {/* Basic Modal */}
+      {isModalOpen && (
+  <div className='modal'>
+    <div className='modal-content'>
+      <span className='close' onClick={closeModal}>&times;</span>
+      <p>Finalizando estadia do cliente {getPlacaById(itemIdToFinalize)} ({getCpfCnpjById(itemIdToFinalize)})?</p>
+      <button onClick={confirmFinalize}>Yes</button>
+      <button onClick={closeModal}>No</button>
+    </div>
+  </div>
+)}
     </div>
   );
 };
